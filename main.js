45244bff94cab5ab3458185b8e5b49e4139ea324
Made <122382388+Marselllniiboyy@users.noreply.github.com>
2024-07-26 22:44:15 +0800

    done i think

--- main.js
+++ main.js
@@ -1,20 +1,49 @@
 // lest gooo!!!
 const container = document.querySelector("#container");
-const gridSize = 16;
+const btnSize = document.querySelector("#btnSize");
+let gridSize = 16;
 
 //grid colom
-for (let i = 0; i < gridSize; i++) {
-  for (let j = 0; j < gridSize; j++) {
-    const page = document.createElement("div");
-    page.classList.add("square");
-    // Hover
-    page.addEventListener("mouseover", function () {
-      this.style.backgroundColor = "red";
-    });
-    // leave Hovere
-    page.addEventListener("mouseleave", function () {
-      this.style.backgroundColor = "red";
-    });
-    container.appendChild(page);
+function createdGrid(size) {
+  container.innerHTML = "";
+  const squareSize = ` calc(100% / ${size})`;
+
+  for (let i = 0; i < size; i++) {
+    console.log(i);
+    for (let j = 0; j < size; j++) {
+      console.log(j);
+      const page = document.createElement("div");
+      page.classList.add("square");
+      page.style.width = squareSize;
+      page.style.height = squareSize;
+      // Hover
+      page.addEventListener("mouseover", function () {
+        this.style.backgroundColor = "red";
+      });
+      // leave Hovere
+      page.addEventListener("mouseleave", function () {
+        this.style.backgroundColor = "red";
+      });
+      container.appendChild(page);
+    }
   }
 }
+
+// btn cngae colom
+function changeGrid() {
+  let inputGrid = Number(prompt("Enter number of squares.."));
+  console.log(inputGrid);
+  if (inputGrid > 100 || inputGrid < 0) {
+    alert("Your input cant be more than 100 and less than 0");
+    gridSize = 0;
+  } else {
+    gridSize = Number(inputGrid);
+  }
+  createdGrid(gridSize);
+}
+
+btnSize.addEventListener("click", function () {
+  changeGrid();
+});
+
+createdGrid();
